8da6b296d0c04452cc660c12d07a4c67cbdd680b
JeanJesus <dyjesus29@hotmail.com>
2018-11-01 16:16:29 -0300

    Organização de pastas e arquivos

---
+++ View/inicial.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body style="background-image:url('img/Background_Login.jpg'); background-size:cover;">
+    <?php include_once('header.php');          ?>
+    <?php include_once('cabecalho.php');       ?>
+    <?php include_once('menu_lateral.php');    ?>
+    <?php include_once('rotas.php');           ?>
+
+
+
+
+
+
+  </body>
+</html>
---
+++ View/menu_lateral.php
@@ -0,0 +1,67 @@
+<div id="wrapper">
+
+  <!-- Sidebar -->
+
+  <ul class="sidebar navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="inicial.php">
+        <i class="fas fa-fw fa fa-home"></i>
+        <span>Início</span>
+      </a>
+    </li>
+
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-fw fa fa-address-card"></i>
+        <span>Cadastrar</span>
+      </a>
+      <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+        <h6 class="dropdown-header">Cadastrar:</h6>
+        <a class="dropdown-item" href="inicial.php?item=CadastrarCliente">Clientes</a>
+        <a class="dropdown-item" href="inicial.php?item=CadastrarAdvogados">Advogados</a>
+        <a class="dropdown-item" href="inicial.php?item=CadastrarAgendamento">Agendamentos</a>
+        <div class="dropdown-divider"></div>
+        <h6 class="dropdown-header">Other Pages:</h6>
+
+        <a class="dropdown-item" href="inicial.php?item=registro_user">Cadastrar Usuário</a>
+        <a class="dropdown-item" href="index.php">Login</a>
+        <a class="dropdown-item" href="forgot-password.php">Forgot Password</a>
+      </div>
+    </li>
+
+  <!--  <li class="nav-item">
+      <a class="nav-link" href="tables.php">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Tables</span></a>
+    </li> -->
+
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Consultar</span></a>
+      </a>
+      <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+        <h6 class="dropdown-header">Listar:</h6>
+        <a class="dropdown-item" href="inicial.php?item=Listar_Clientes">Clientes</a>
+        <a class="dropdown-item" href="inicial.php?item=Listar_Advogados">Advogados</a>
+        <a class="dropdown-item" href="inicial.php?item=Listar_Agendamentos">Agendamentos</a>
+        <div class="dropdown-divider"></div>
+
+
+      </div>
+    </li>
+
+
+
+    <!--  -->
+    <li class="nav-item">
+      <a class="nav-link" href="inicial.php?item=charts">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Charts</span></a>
+    </li>
+
+  </ul>
+
+  <div id="content-wrapper">
+
+    <div class="container-fluid">
